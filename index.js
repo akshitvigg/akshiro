2e13a1bf158534764254355687e3fd9f7c456e1c
Akshit Vig <akshitvig48@gmail.com>
2024-12-10 20:43:38 +0530

    testing

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 #!/usr/bin/ env node
+
+import chalk, { chalkStderr } from "chalk";
+
+import figlet from "figlet";
+import boxen from "boxen";
+
+console.log(chalk.bgCyan(chalkStderr.red("Hello Akshit")));
