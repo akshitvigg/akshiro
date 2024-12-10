341d31eaa7787cede6667cf7878e9a48e6237b1c
Akshit Vig <akshitvig48@gmail.com>
2024-12-10 21:25:10 +0530

    build

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 import chalk from "chalk";
 import figlet from "figlet";
 import boxen from "boxen";
+import terminalImage from "terminal-image";
+import got from "got";
 
-figlet.text(
-  "もしもし.",
-  {
-    font: "Katakana",
-    horizontalLayout: "default",
-    verticalLayout: "default",
-    width: 80,
-    whitespaceBreak: true,
-  },
-  (err, data) => {
-    if (err) {
-      console.log("something went wrong", err);
-      return;
-    }
-    console.log(data);
-  }
-);
+// figlet.text(
+//   "もしもし.",
+//   {
+//     font: "Katakana",
+//     horizontalLayout: "default",
+//     verticalLayout: "default",
+//     width: 80,
+//     whitespaceBreak: true,
+//   },
+//   (err, data) => {
+//     if (err) {
+//       console.log("something went wrong", err);
+//       return;
+//     }
+//     console.log(data);
+//   }
+// );
+
+const body = await got();
+console.log(await terminalImage.file);
